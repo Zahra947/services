b63993638f5617887881e921486862aec5e6a527
Zahra Rabiei <85116470+Zahra947@users.noreply.github.com>
2022-02-08 16:05:08 +0330

    trying to fix the problem

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import { Router, Routes, Route, Link } from "react-router-dom";
+import { Router, Routes, Route } from "react-router-dom";
 
 import "./styles.css";
 import Header from "./pages/Header";
@@ -12,20 +12,6 @@ import Home from "./pages/Home";
 function App() {
   return (
     <Router>
-      <div>
-        <li>
-          <link to="/">Home</link>
-        </li>
-        <li>
-          <link to="/services">Services</link>
-        </li>
-        <li>
-          <link to="/about">About</link>
-        </li>
-        <li>
-          <link to="/contact">Contact</link>
-        </li>
-      </div>
       <Routes>
         <Route path="header" element={<Header />} />
         <Route index element={<Home />} />
@@ -36,7 +22,6 @@ function App() {
     </Router>
   );
 }
-
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
