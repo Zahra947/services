125c692a1a57232175a3c4e202001b8a542ffc47
Zahra Rabiei <85116470+Zahra947@users.noreply.github.com>
2022-02-09 13:25:25 +0330

    corrected the path

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import Header from "./pages/Header";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Services from "./pages/Services";
+//import Home from "./pages/Home";
 
 export default function App() {
-  const Home = () => <div>Home page </div>;
+  const Home = () => <div>hi</div>;
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/services" element={<Services />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
