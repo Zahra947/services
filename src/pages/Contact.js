104ba96beb6257b476544dbf08ea1beeb8a86d72
Zahra Rabiei <85116470+Zahra947@users.noreply.github.com>
2022-02-16 15:20:52 +0330

    finished contact page added divs and contents

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,8 +3,19 @@ import React from "react";
 export default function Contact() {
   return (
     <div className="container-fluid">
-      <h1>hello</h1>
-      <h1>contact</h1>
+      <div className="img-fluid d-flex align-items-sm-center justify-content-sm-center hfonts contact-1 p-5">
+        <span className="contact-11"> Contatc Us</span>
+      </div>
+      <div className="d-flex align-items-sm-center justify-content-sm-center">
+        <ul className="liststyle-1">
+          <li>Email: </li>
+          <li>Phone: </li>
+          <li>Address:</li>
+          <li>Facebook:</li>
+          <li>Instagram:</li>
+          <li>Twitter:</li>
+        </ul>
+      </div>
     </div>
   );
 }
