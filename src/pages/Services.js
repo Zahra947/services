f460118b2b0b554b09627fbcce5714033e1b3e4a
Zahra Rabiei <85116470+Zahra947@users.noreply.github.com>
2022-02-16 12:32:28 +0330

    added more divs with ids to services page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -7,15 +7,27 @@ export default function Services() {
         Find your&nbsp;<span className="services-11">services</span>&nbsp;here
       </div>
       <div className="row d-flex">
-        <div className="col-4 d-flex align-items-sm-center justify-content-sm-center">
-          service 1
+        <div className="col-6 d-flex align-items-sm-center justify-content-sm-center">
+          <a href="#online" title="Online Services">
+            <img
+              src="https://images.pexels.com/photos/6446676/pexels-photo-6446676.jpeg?cs=srgb&dl=pexels-karina-zhukovskaya-6446676.jpg&fm=jpg"
+              alt="online services"
+              className="img-fluid p-3"
+            />
+          </a>
         </div>
-        <div className="col-4 d-flex align-items-sm-center justify-content-sm-center">
-          service 2
+        <div className="col-6 d-flex align-items-sm-center justify-content-sm-center">
+          <a href="#in-person" title="In-Person Services">
+            <img
+              src="https://images.pexels.com/photos/3184416/pexels-photo-3184416.jpeg?cs=srgb&dl=pexels-fauxels-3184416.jpg&fm=jpg"
+              alt="online services"
+              className="img-fluid p-3"
+            />
+          </a>
         </div>
-        <div className="col-4 d-flex align-items-sm-center justify-content-sm-center">
-          service 3
-        </div>
+      </div>
+      <div className="online-services p-5" id="online">
+        online services
       </div>
     </div>
   );
