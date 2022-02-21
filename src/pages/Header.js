45d175966d2823c24765d78ec46c17bcb8c4e3e4
Zahra Rabiei <85116470+Zahra947@users.noreply.github.com>
2022-02-21 14:47:48 +0330

    added titles to the header links

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -9,18 +9,19 @@ export default function Header() {
       <nav className="navbar navbar-expand-lg navbar-light d-flex justify-content-sm-center header">
         <Link
           to="/Home"
-          className="header p-3 nav-link active"
+          className="nav-link active header p-3 "
           aria-current="page"
+          title="Home"
         >
           Home
         </Link>
-        <Link to="/Services" className="header p-3 nav-link">
+        <Link to="/Services" className="header p-3 nav-link" title="Services">
           Services
         </Link>
-        <Link to="/About" className="header p-3 nav-link">
+        <Link to="/About" className="header p-3 nav-link" title="About">
           About
         </Link>
-        <Link to="/Contact" className="header p-3 nav-link">
+        <Link to="/Contact" className="header p-3 nav-link" title="Contact">
           Contact
         </Link>
       </nav>
