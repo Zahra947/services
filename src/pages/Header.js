d2109a5fdb0d30d000af6f50b6159cc68d69339c
Zahra Rabiei <85116470+Zahra947@users.noreply.github.com>
2022-02-21 14:39:54 +0330

    trying to make have responsible header

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -6,17 +6,21 @@ import "../styles/Header.scss";
 export default function Header() {
   return (
     <div className="container-fluid">
-      <nav className="navbar navbar-expand-lg navbar-light d-flex justify-content-sm-center header ">
-        <Link to="/Home" className="header p-3 active">
+      <nav className="navbar navbar-expand-lg navbar-light d-flex justify-content-sm-center header">
+        <Link
+          to="/Home"
+          className="header p-3 nav-link active"
+          aria-current="page"
+        >
           Home
         </Link>
-        <Link to="/Services" className="header p-3">
+        <Link to="/Services" className="header p-3 nav-link">
           Services
         </Link>
-        <Link to="/About" className="header p-3">
+        <Link to="/About" className="header p-3 nav-link">
           About
         </Link>
-        <Link to="/Contact" className="header p-3">
+        <Link to="/Contact" className="header p-3 nav-link">
           Contact
         </Link>
       </nav>
