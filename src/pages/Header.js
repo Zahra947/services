1923ade52460af764cf41c3fbe1e9fc72cfbe423
Zahra Rabiei <85116470+Zahra947@users.noreply.github.com>
2022-02-21 14:14:50 +0330

    removed unnecessary buttons

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -7,62 +7,18 @@ export default function Header() {
   return (
     <div className="container-fluid">
       <nav className="navbar navbar-expand-lg navbar-light d-flex justify-content-sm-center header ">
-        <Link to="/Home" className="header p-3">
+        <Link to="/Home" className="header p-3 active">
           Home
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
         <Link to="/Services" className="header p-3">
           Services
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
         <Link to="/About" className="header p-3">
           About{" "}
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
         <Link to="/Contact" className="header p-3">
           Contact
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
       </nav>
     </div>
   );
