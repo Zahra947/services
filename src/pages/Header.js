9c9728b15fe2cbef97c76af89b054938d19c56c2
Zahra Rabiei <85116470+Zahra947@users.noreply.github.com>
2022-02-22 13:29:01 +0330

    fixwd header

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -6,30 +6,38 @@ import "../styles/style.css";
 
 export default function Header() {
   return (
-    <div className="container-fluid">
-      <nav className="navbar d-flex justify-content-sm-center header">
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <Link
-              to="/Home"
-              className="p-3 nav-link active"
-              aria-current="page"
-              title="Home"
-            >
-              Home
-            </Link>
-            <Link to="/Services" className="p-3 nav-link" title="Services">
-              Services
-            </Link>
-            <Link to="/About" className="p-3 nav-link" title="About">
-              About
-            </Link>
-            <Link to="/Contact" className="p-3 nav-link" title="Contact">
-              Contact
-            </Link>
+    <nav class="navbar navbar-expand-lg d-flex justify-content-sm-center header">
+      <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <div class="navbar-nav d-flex justify-content-sm-center header">
+            <span class="nav-item">
+              <Link
+                to="/Home"
+                class="p-3 nav-link active"
+                aria-current="page"
+                title="Home"
+              >
+                Home
+              </Link>
+            </span>
+            <span class="nav-item">
+              <Link to="/Services" className="nav-link" title="Services">
+                Services
+              </Link>
+            </span>
+            <span class="nav-item">
+              <Link to="/About" className="nav-link" title="About">
+                About
+              </Link>
+            </span>
+            <span class="nav-item">
+              <Link to="/Contact" className="nav-link" title="Contact">
+                Contact
+              </Link>
+            </span>
           </div>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 }
