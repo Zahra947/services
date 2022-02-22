c44ea0811202c2f7f9ea2610123b58b25fe05e56
Zahra Rabiei <85116470+Zahra947@users.noreply.github.com>
2022-02-22 12:09:39 +0330

    modified header

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -8,23 +8,27 @@ export default function Header() {
   return (
     <div className="container-fluid">
       <nav className="navbar d-flex justify-content-sm-center header">
-        <Link
-          to="/Home"
-          className=" header p-3 nav-link active"
-          aria-current="page"
-          title="Home"
-        >
-          Home
-        </Link>
-        <Link to="/Services" className="header p-3 nav-link" title="Services">
-          Services
-        </Link>
-        <Link to="/About" className="header p-3 nav-link" title="About">
-          About
-        </Link>
-        <Link to="/Contact" className="header p-3 nav-link" title="Contact">
-          Contact
-        </Link>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <Link
+              to="/Home"
+              className="p-3 nav-link active"
+              aria-current="page"
+              title="Home"
+            >
+              Home
+            </Link>
+            <Link to="/Services" className="p-3 nav-link" title="Services">
+              Services
+            </Link>
+            <Link to="/About" className="p-3 nav-link" title="About">
+              About
+            </Link>
+            <Link to="/Contact" className="p-3 nav-link" title="Contact">
+              Contact
+            </Link>
+          </div>
+        </div>
       </nav>
     </div>
   );
